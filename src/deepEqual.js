957605da5916f4ba48f98ba572efcc17f526cf6b
devmian <devmian@gmail.com>
2023-06-07 14:25:35 +1000

    Add files via upload

---
+++ src/deepEqual.js
@@ -0,0 +1,28 @@
+const deepEqual = (a, b) => {
+  if (a === null && b === null) {
+    return true;
+  }
+
+  if (a === undefined && b === undefined) {
+    return true;
+  }
+
+  if (
+    a !== null &&
+    b !== null &&
+    typeof a === "object" &&
+    typeof b === "object"
+  ) {
+    if (Object.keys(a).length >= Object.keys(b).length) {
+      return compareTwoObject(a, b);
+    }
+    return compareTwoObject(b, a);
+  }
+
+  return false;
+};
+
+const compareTwoObject = (a, b) =>
+  Object.keys(a).every((k) => (a[k] === undefined ? true : a[k] === b[k]));
+
+module.exports = deepEqual;
---
+++ src/deepEqual.test.js
@@ -0,0 +1,40 @@
+const { expect } = require("chai");
+const deepEqual = require("./deepEqual");
+
+describe("deepEqual", () => {
+  it("should return true given undefined, undefined", () => {
+    expect(deepEqual(undefined, undefined)).to.equal(true);
+  });
+
+  it("should return true given two value equal objects", () => {
+    expect(deepEqual({ name: "Bob" }, { name: "Bob" })).to.equal(true);
+  });
+
+  it("should return false given undefined, null", () => {
+    expect(deepEqual(undefined, null)).to.equal(false);
+  });
+
+  it("should return true given two value equal objects with undefined", () => {
+    expect(
+      deepEqual({ name: "Bob", email: undefined }, { name: "Bob" })
+    ).to.equal(true);
+  });
+
+  it("should return false given two value equal objects with null", () => {
+    expect(deepEqual({ name: "Bob", email: null }, { name: "Bob" })).to.equal(
+      false
+    );
+  });
+
+  it("should return false given two value different objects", () => {
+    expect(
+      deepEqual({ name: "Bob", email: "dev@gmail.com" }, { name: "Bob" })
+    ).to.equal(false);
+  });
+
+  it("should return false given two value object, nul", () => {
+    expect(deepEqual({ name: "Bob", email: "dev@gmail.com" }, null)).to.equal(
+      false
+    );
+  });
+});
